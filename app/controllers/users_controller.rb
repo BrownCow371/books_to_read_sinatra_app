bc8671d0081eab3b00846134f44d592924efbd19
Jamie Brown <jmbrowncow@gmail.com>
2018-08-16 15:47:29 +0000

    add signup route to usercontroller, add view folders for users and books and new_user file to users views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+  get '/signup' do
+    if logged_in?
+      redirect "/users/#{current_user.id}"
+    else
+      erb :'users/new_user'
+    end
+  end
+
+
+
 end

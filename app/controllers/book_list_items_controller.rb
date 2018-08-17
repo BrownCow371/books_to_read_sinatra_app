9eb18f49f75f98759b2dfa1f7ccaa1bea4f57acf
Jamie Brown <jmbrowncow@gmail.com>
2018-08-17 20:27:46 +0000

    remove book new.erb - will not be needed, add blank edit and show erbs for book_list_items, fix create book_list_item logic in /book_list_items route

--- app/controllers/book_list_items_controller.rb
+++ app/controllers/book_list_items_controller.rb
@@ -2,7 +2,8 @@ class BookListItemsController < ApplicationController
 
   get '/book_list_items/new' do
      if logged_in?
-      @books=Book.all.select{|book| !(current_user.books.include?(book))}
+       @categories = Category.all
+       @books=Book.all.select{|book| !(current_user.books.include?(book))}
       erb :'book_list_items/new'
      else
       erb :'users/login'
@@ -10,14 +11,27 @@ class BookListItemsController < ApplicationController
   end
 
   post '/book_list_items' do
-    # binding.pry
-    if params[:user_id].to_i == current_user.id
-      params[:book_list_item][:book_ids].each do |id|
+    #  binding.pry
+
+    if params[:book_list_item][:user_id].to_i == current_user.id
+      if params[:check_list_items][:book_ids]
+        params[:check_list_items][:book_ids].each do |id|
         bli = BookListItem.new(book_id: id, user_id: current_user.id)
-        bli.note = params[:book_list_item]["book_id_#{id}"][:note]
-        bli.library_link = params[:book_list_item]["book_id_#{id}"][:library_link]
+        bli.note = params[:check_list_items]["book_id_#{id}"][:note]
+        bli.library_link = params[:check_list_items]["book_id_#{id}"][:library_link]
         bli.save
-        # binding.pry
+        end
+      end
+      # need to check to see if new book section was filledin
+      # need to make sure new book is valid before save
+      if !params[:book].empty? && Book.new(params[:book]).valid?
+        book = Book.find_or_create_by(params[:book])
+        new_list_item = BookListItem.new(params[:book_list_item])
+        new_list_item.book_id=book.id
+        new_list_item.save
+      else
+        flash[:message] = "When creating a new book, please provide both a tilte and an author."
+        redirect '/book_list_items/new'
       end
       redirect "/users/#{current_user.id}"
     else
@@ -28,8 +42,6 @@ class BookListItemsController < ApplicationController
 
 
   delete '/book_list_items/:id/delete' do
-   #  this does not delete books. This removes the link between a user and a book -
-   #  ie. this removes the book from the user's book list
    list_item = BookListItem.find_by_id(params[:id])
     if current_user.id == list_item.user_id
       flash[:message] = "We have removed #{list_item.book.title} by #{list_item.book.author} from your book list."

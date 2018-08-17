8ed1e2f2ad79de9deaa23d13fb93105c18575953
Jamie Brown <jmbrowncow@gmail.com>
2018-08-17 16:19:16 +0000

    add logged_in? logic to book index page, add delete book_list_item route to book controller, fix button formatting on user show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,28 @@ class BooksController < ApplicationController
  # users should only be able to add one book list itme per book (so only one set of notes/book location per book)
 
  get '/books' do
-   @books=Book.all
-   erb :'books/index'
+   if logged_in?
+     @books=Book.all
+     erb :'books/index'
+   else
+     erb :'users/login'
+   end
+ end
+
+ get '/books/add_book' do
+    if logged_in?
+     @books=Book.all
+     erb :'books/add_book'
+    else
+     erb :'users/login'
+    end
+ end
+
+ delete '/books/:id/delete' do
+  #  binding.pry
+  #  @user = current_user
+  #  @book = Book.find_by_id(params[:id])
+   current_user.book_list_items.find_by(book_id: params[:id]).destroy
+   redirect "/users/#{current_user.id}"
  end
 end
